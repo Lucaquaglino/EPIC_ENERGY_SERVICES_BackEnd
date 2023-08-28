fb9b6d260c13d57495de1995f86512e934614d1f
Luca Quaglino <quagli.luca@gmail.com>
2023-08-28 13:44:29 +0200

    creazione struttura progetto

---
+++ EPIC_ENERGY_SERVICES_BackEnd/src/main/java/EPIC_ENERGY_SERVICES_BackEnd/EpicEnergyServicesBackEndApplication.java
@@ -0,0 +1,13 @@
+package EPIC_ENERGY_SERVICES_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicEnergyServicesBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicEnergyServicesBackEndApplication.class, args);
+	}
+
+}
---
+++ EPIC_ENERGY_SERVICES_BackEnd/src/test/java/EPIC_ENERGY_SERVICES_BackEnd/EpicEnergyServicesBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package EPIC_ENERGY_SERVICES_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicEnergyServicesBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
