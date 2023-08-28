69aee09938dc1f0a9bdae96471e65bda475b3835
Luca Quaglino <quagli.luca@gmail.com>
2023-08-28 15:54:27 +0200

    Aggiunta REPOSITORY entities comune provincia indirizzo

---
+++ EPIC_ENERGY_SERVICES_BackEnd/src/main/java/EPIC_ENERGY_SERVICES_BackEnd/entities/comune/ComuneRepository.java
@@ -0,0 +1,7 @@
+package EPIC_ENERGY_SERVICES_BackEnd.entities.comune;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ComuneRepository extends JpaRepository<Comune, Integer> {
+
+}
