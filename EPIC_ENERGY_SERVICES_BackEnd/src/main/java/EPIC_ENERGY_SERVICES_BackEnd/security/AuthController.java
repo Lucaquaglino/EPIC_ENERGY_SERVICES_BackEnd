920c3d90930b1910a26008585cc2327ef4b9a095
Marcotmn <129113184+Marcotmn@users.noreply.github.com>
2023-08-28 16:06:42 +0200

    implementazione sistema auth
    login ecc ecc

---
+++ EPIC_ENERGY_SERVICES_BackEnd/src/main/java/EPIC_ENERGY_SERVICES_BackEnd/security/AuthController.java
@@ -0,0 +1,53 @@
+package EPIC_ENERGY_SERVICES_BackEnd.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import EPIC_ENERGY_SERVICES_BackEnd.utente.Utente;
+import EPIC_ENERGY_SERVICES_BackEnd.exceptions.UnauthorizedException;
+import EPIC_ENERGY_SERVICES_BackEnd.utente.LoginSuccessfullPayload;
+import EPIC_ENERGY_SERVICES_BackEnd.utente.NuovoUtentePayload;
+import EPIC_ENERGY_SERVICES_BackEnd.utente.UtenteLoginPayload;
+import EPIC_ENERGY_SERVICES_BackEnd.utente.UtenteService;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+	@Autowired
+	UtenteService utenteService;
+
+	@Autowired
+	JWTTools jwtTools;
+
+	@Autowired
+	PasswordEncoder bcrypt;
+
+	@PostMapping("/register")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Utente saveUser(@RequestBody NuovoUtentePayload body) {
+
+		body.setPassword(bcrypt.encode(body.getPassword()));
+		Utente created = utenteService.save(body);
+		return created;
+	}
+
+	@PostMapping("/login")
+	public LoginSuccessfullPayload login(@RequestBody UtenteLoginPayload body) {
+
+		Utente utente = utenteService.findByEmail(body.getEmail());
+
+		if (bcrypt.matches(body.getPassword(), utente.getPassword())) {
+			String token = jwtTools.createToken(utente);
+			return new LoginSuccessfullPayload(token);
+		} else {
+			throw new UnauthorizedException("Credenziali non valide");
+		}
+	}
+}
---
+++ EPIC_ENERGY_SERVICES_BackEnd/src/main/java/EPIC_ENERGY_SERVICES_BackEnd/security/CorsFilter.java
@@ -0,0 +1,33 @@
+package EPIC_ENERGY_SERVICES_BackEnd.security;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@Component
+public class CorsFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+
+       
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            filterChain.doFilter(request, response);
+        }
+    }
+}
