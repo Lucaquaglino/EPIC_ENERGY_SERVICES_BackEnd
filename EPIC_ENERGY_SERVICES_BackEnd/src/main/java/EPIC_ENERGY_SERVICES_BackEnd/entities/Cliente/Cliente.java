5e5d6fd5140262d1146ac4091e42b7d65596dbe4
EazyM93 <centini.manuel.93@gmail.com>
2023-08-28 15:04:16 +0200

    Creazione attributi

--- EPIC_ENERGY_SERVICES_BackEnd/src/main/java/EPIC_ENERGY_SERVICES_BackEnd/entities/Cliente/Cliente.java
+++ EPIC_ENERGY_SERVICES_BackEnd/src/main/java/EPIC_ENERGY_SERVICES_BackEnd/entities/Cliente/Cliente.java
@@ -1,5 +1,9 @@
 package EPIC_ENERGY_SERVICES_BackEnd.entities.Cliente;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -17,5 +21,26 @@ public class Cliente {
 
 	@Id
 	@GeneratedValue
+	private UUID id_cliente;
+	
+	//dati cliente
+	private String ragione_sociale;
+	private int partita_iva;
+	private String email_cliente;
+	private String pec;
+	private int telefono_cliente;
+	private List<String> indirizzi;
+	private TIPO_CLIENTE tipo_cliente;
+	
+	//dati contatto
+	private String nome_contatto;
+	private String cognome_contatto;
+	private String email_contatto;
+	private int telefono_contatto;
+	
+	//attributi inizializzati alla crazione
+	private LocalDate data_inserimento;
+	private double fatturato_annuale;
+	private LocalDate ultimo_contatto; 
 	
 }
