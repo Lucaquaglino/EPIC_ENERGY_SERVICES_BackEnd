012f2d19fbc0a7a3056fec69f5f9a04ba1005816
EazyM93 <centini.manuel.93@gmail.com>
2023-08-28 14:50:07 +0200

    Annotazioni Cliente Entity

--- EPIC_ENERGY_SERVICES_BackEnd/src/main/java/EPIC_ENERGY_SERVICES_BackEnd/entities/Cliente/Cliente.java
+++ EPIC_ENERGY_SERVICES_BackEnd/src/main/java/EPIC_ENERGY_SERVICES_BackEnd/entities/Cliente/Cliente.java
@@ -1,5 +1,16 @@
 package EPIC_ENERGY_SERVICES_BackEnd.entities.Cliente;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Data;
+
+
+@Entity
+@Builder
+@Data
+@Table(name="clienti")
+
 public class Cliente {
 
 }
