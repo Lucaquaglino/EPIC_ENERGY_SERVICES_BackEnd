86e2196bb4ea138df584b4ced9cfe64f8a375e72
EazyM93 <centini.manuel.93@gmail.com>
2023-08-28 15:22:16 +0200

    Update ClienteRepository.java

--- EPIC_ENERGY_SERVICES_BackEnd/src/main/java/EPIC_ENERGY_SERVICES_BackEnd/entities/Cliente/ClienteRepository.java
+++ EPIC_ENERGY_SERVICES_BackEnd/src/main/java/EPIC_ENERGY_SERVICES_BackEnd/entities/Cliente/ClienteRepository.java
@@ -1,5 +1,11 @@
 package EPIC_ENERGY_SERVICES_BackEnd.entities.Cliente;
 
-public interface ClienteRepository {
+import java.util.UUID;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, UUID>{
+	
 }
