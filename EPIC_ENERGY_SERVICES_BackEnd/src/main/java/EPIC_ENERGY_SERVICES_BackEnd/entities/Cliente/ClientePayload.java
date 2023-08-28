860af044215cf180653b30ea304de79f0190b46d
EazyM93 <centini.manuel.93@gmail.com>
2023-08-28 15:25:40 +0200

    Creazione Cliente Payload

---
+++ EPIC_ENERGY_SERVICES_BackEnd/src/main/java/EPIC_ENERGY_SERVICES_BackEnd/entities/Cliente/ClientePayload.java
@@ -0,0 +1,12 @@
+package EPIC_ENERGY_SERVICES_BackEnd.entities.Cliente;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ClientePayload {
+
+}
--- EPIC_ENERGY_SERVICES_BackEnd/src/main/java/EPIC_ENERGY_SERVICES_BackEnd/entities/Cliente/ClienteService.java
+++ EPIC_ENERGY_SERVICES_BackEnd/src/main/java/EPIC_ENERGY_SERVICES_BackEnd/entities/Cliente/ClienteService.java
@@ -1,5 +1,16 @@
 package EPIC_ENERGY_SERVICES_BackEnd.entities.Cliente;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ClienteService {
 
+		@Autowired
+		ClienteRepository cr;
+		
+		public Cliente create() {
+			
+		}
+	
 }
