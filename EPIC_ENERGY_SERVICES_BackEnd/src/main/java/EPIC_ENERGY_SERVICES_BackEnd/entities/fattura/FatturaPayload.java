cb8c7a46fd6f2f64bfb1772040775c83ac8a634d
Emanuele <126979214+Emanuele-Murru@users.noreply.github.com>
2023-08-30 18:54:39 +0200

    Update FatturaPayload.java

--- EPIC_ENERGY_SERVICES_BackEnd/src/main/java/EPIC_ENERGY_SERVICES_BackEnd/entities/fattura/FatturaPayload.java
+++ EPIC_ENERGY_SERVICES_BackEnd/src/main/java/EPIC_ENERGY_SERVICES_BackEnd/entities/fattura/FatturaPayload.java
@@ -3,6 +3,8 @@ package EPIC_ENERGY_SERVICES_BackEnd.entities.fattura;
 import java.time.LocalDate;
 import java.util.UUID;
 
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -12,6 +14,7 @@ public class FatturaPayload {
 	private int anno;
 	private LocalDate data;
 	private double importo;
+	@Enumerated(EnumType.STRING)
 	private StatoFattura statoFattura;
 	private UUID id_cliente;
 }
