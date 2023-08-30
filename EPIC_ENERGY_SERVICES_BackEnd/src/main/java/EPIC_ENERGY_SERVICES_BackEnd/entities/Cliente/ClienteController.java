e8288f43f5a6b780519721054215fbd030d77dd5
EazyM93 <centini.manuel.93@gmail.com>
2023-08-30 10:51:38 +0200

    Update ClienteController.java

--- EPIC_ENERGY_SERVICES_BackEnd/src/main/java/EPIC_ENERGY_SERVICES_BackEnd/entities/Cliente/ClienteController.java
+++ EPIC_ENERGY_SERVICES_BackEnd/src/main/java/EPIC_ENERGY_SERVICES_BackEnd/entities/Cliente/ClienteController.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -33,6 +32,7 @@ public class ClienteController {
 		return clienteCreato;
 	}
 
+	//--------------------------------------------------------------------------- ordinamenti
 	@GetMapping("")
 	public Page<Cliente> findAll(
 			@RequestParam(defaultValue = "0") int page,
