6a2353a6997f0500c45ce898bef776bb35dcde4a
EazyM93 <centini.manuel.93@gmail.com>
2023-08-30 11:37:30 +0200

    Update Fattura.java

--- EPIC_ENERGY_SERVICES_BackEnd/src/main/java/EPIC_ENERGY_SERVICES_BackEnd/entities/fattura/Fattura.java
+++ EPIC_ENERGY_SERVICES_BackEnd/src/main/java/EPIC_ENERGY_SERVICES_BackEnd/entities/fattura/Fattura.java
@@ -8,14 +8,15 @@ import EPIC_ENERGY_SERVICES_BackEnd.entities.Cliente.Cliente;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Fattura {
@@ -25,19 +26,10 @@ public class Fattura {
 	private int anno;
 	private LocalDate data;
 	private BigDecimal importo;
-	private double numeroFattura = 0;
-	
-	@ManyToMany
+	final private double numeroFattura = 0;
 	private StatoFattura statoFattura;
 	
 	@ManyToOne
 	private Cliente cliente;
 	
-	public Fattura(int _anno, LocalDate _data, BigDecimal _importo, StatoFattura _statoFattura, Cliente _cliente) {
-		this.anno = _anno;
-		this.data = _data;
-		this.importo = _importo;
-		this.statoFattura = _statoFattura;
-		this.cliente = _cliente;
-	}
 }
