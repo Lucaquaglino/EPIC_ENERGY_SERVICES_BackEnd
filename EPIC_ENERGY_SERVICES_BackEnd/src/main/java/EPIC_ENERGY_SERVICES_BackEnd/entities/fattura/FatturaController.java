1ef2b1c9be05db8c8c7cfb9b64dbc3fb3432ff02
EazyM93 <centini.manuel.93@gmail.com>
2023-08-31 10:27:24 +0200

    Update FatturaController.java

--- EPIC_ENERGY_SERVICES_BackEnd/src/main/java/EPIC_ENERGY_SERVICES_BackEnd/entities/fattura/FatturaController.java
+++ EPIC_ENERGY_SERVICES_BackEnd/src/main/java/EPIC_ENERGY_SERVICES_BackEnd/entities/fattura/FatturaController.java
@@ -1,17 +1,22 @@
 package EPIC_ENERGY_SERVICES_BackEnd.entities.fattura;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/fattura")
@@ -34,6 +39,19 @@ public class FatturaController {
 
 		return fatturaCreata;
 
+	}
+	
+	//--------------------------------------------------------------------------- trova per id
+	@GetMapping("/{id}")
+	public Fattura findById(@PathVariable UUID id) throws NotFoundException {
+		return fs.findById(id);
+	}
+		
+	//--------------------------------------------------------------------------- elimina
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public void deleteById(@PathVariable UUID id) throws NotFoundException {
+		fs.findByIdAndDelete(id);
 	}
 
 	//--------------------------------------------------------------------------- filtro per cliente
